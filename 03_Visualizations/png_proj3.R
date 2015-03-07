231763611b80cf8cd3f5ce8a43bf614cc7e5931c
Sarah Stancik <skstancik@utexas.edu>
2015-03-06 20:54:48 -0600

    categorical 1 working

--- 03_Visualizations/png_proj3.R
+++ 03_Visualizations/png_proj3.R
@@ -1,18 +1,16 @@
-# define myplot
+# categoricals
 myplot <- function(df, x) {
   names(df) <- c("x", "n")
   ggplot(df, aes(x=x, y=n)) + geom_point()
 }
 
-# categorical for margin of error
-# categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from dfINNER"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
+categoricals <- eval(parse(text=substring(gsub(",)", ")", getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from COMMUTE_TIMES"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE)), 1, 2^31-1)))
 
-categoricals <- eval(parse(text=substring(gsub(",)", ")", getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from MARGIN_OF_ERROR"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_sks2435', PASS='orcl_sks2435', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE)), 1, 2^31-1)))
 
 l <- list()
-for (i in names(dfINNER)) { 
+for (i in names(dfCOMMUTE)) { 
   if (i %in% categoricals[[1]]) {
-    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from MARGIN_OF_ERROR group by \\\""i"\\\""'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_sks2435',PASS='orcl_sks2435',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from COMMUTE_TIMES group by \\\""i"\\\""'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_sks2435',PASS='orcl_sks2435',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
     p <- myplot(r,i)
     print(p) 
     l[[i]] <- p
@@ -25,7 +23,7 @@ pushViewport(viewport(layout = grid.layout(1, 12)))
 
 print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:4))
 print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 5:8))
-print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 9:12))
+print(l[[3]], vp = viewport(layout.pos.row = 2, layout.pos.col = 9:12))
 
 dev.off()
 
@@ -34,28 +32,24 @@ myplot1 <- function(df, x) {
   names(df) <- c("x")
   ggplot(df, aes(x=x)) + geom_histogram()
 }
-l <- list()
-for (i in names(ddf)) {   
-  # For details on [[...]] below, see http://stackoverflow.com/questions/1169456/in-r-what-is-the-difference-between-the-and-notations-for-accessing-the
+
+l1 <- list()
+for (i in names(dfCOMMUTE)) {   
   if (i %in% categoricals[[2]]) {
-    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from diamonds where \\\""i"\\\" is not null "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',USER='DV_Diamonds',PASS='orcl',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from COMMUTE_TIMES"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_sks2435',PASS='orcl_sks2435',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
     p <- myplot1(r,i)
     print(p) 
-    l[[i]] <- p
+    l1[[i]] <- p
   }
 }
 
-#png("../01 Data/categoricals2.png", width = 25, height = 20, units = "in", res = 72)
+png("/Users/sarahstancik/Documents/Development/DataVisualization/DV_RProject3/01_Data/categoricals2.png", width = 25, height = 10, units = "in", res = 72)
 grid.newpage()
 pushViewport(viewport(layout = grid.layout(2, 12)))   
 
-print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:3))
-print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 4:6))
-print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 7:9))
-print(l[[4]], vp = viewport(layout.pos.row = 1, layout.pos.col = 10:12))
-print(l[[5]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:3))
-print(l[[6]], vp = viewport(layout.pos.row = 2, layout.pos.col = 4:6))
-print(l[[7]], vp = viewport(layout.pos.row = 2, layout.pos.col = 7:9))
-print(l[[8]], vp = viewport(layout.pos.row = 2, layout.pos.col = 10:12))
+print(l1[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:3))
+print(l1[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 4:6))
+print(l1[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 7:9))
+print(l1[[4]], vp = viewport(layout.pos.row = 1, layout.pos.col = 10:12))
 
 dev.off()
